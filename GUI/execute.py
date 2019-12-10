02ae1ed42dae6f32210e4178a311feacde965219
DaviDarkFire <daviferreirasantog@gmail.com>
2019-12-10 16:22:19 -0400

    alterações feitas e aparentemente funfando

--- GUI/execute.py
+++ GUI/execute.py
@@ -6,14 +6,7 @@ import glob
 import os
 
 global caixa
-global train #1 para treino e 0 para classificação
 global path
-
-
-
-def fileOrFolder(path): #return 1 if file and 0 if folder
-    return os.path.isfile(path)
-
 
 def convert_jpeg(path): #convert image files to jpg
     for subdir, dirs, files in os.walk(path):
@@ -34,104 +27,82 @@ def classificar(path):
     os.system("python facenet/src/classifier.py CLASSIFY "+os.path.realpath(path)+" "+os.path.realpath("facenet/model/pesos.pb")+" "+os.path.realpath("facenet/model/classifier.pkl")+" --batch_size 1000 > out.txt")
 
 
-
-def clicked_file(widget):
-    global caixa
-    global path
-    file_browser = Fl_File_Chooser(".", "*", Fl_File_Chooser.SINGLE, "Selecione a imagem ou vídeo")
-    #file_browser = Fl_File_Chooser(".", "*", Fl_File_Chooser.DIRECTORY, "Selecione o arquivo de video")
-    file_browser_label = Fl_Box(16,4,25,5, "Arquivo:")
-    file_browser.show()
-    while file_browser.visible():
-        Fl.wait() 
-
-    caixa.box(FL_DOWN_BOX)
-
-    img = Fl_JPEG_Image(file_browser.value())
-    caixa.image(img)
-    caixa.label("")
-
-    caixa.redraw()
-
-    path = file_browser.value()
-
-    
-
-def clicked_folder(widget):
+def handle_folder():
     global path
     file_browser = Fl_File_Chooser(".", "*", Fl_File_Chooser.DIRECTORY, "Selecione a pasta com imagens e/ou vídeos")
     #file_browser = Fl_File_Chooser(".", "*", Fl_File_Chooser.DIRECTORY, "Selecione o arquivo de video")
     file_browser_label = Fl_Box(16,4,25,5, "Pasta")
     file_browser.show()
+
     while file_browser.visible():
         Fl.wait()
-
-    path = file_browser.value()
-
-    caixa.box(FL_DOWN_BOX)
-
-    text = ""
     
+    if(file_browser.value() == None):
+        return
+    
+    path = file_browser.value()
+    text = ""
     for filename in os.listdir(path):
         text = text+filename+"\n"
 
-    caixa.image(None)
+    caixa.box(FL_DOWN_BOX)
     caixa.label(text)
     caixa.redraw()
 
-def clicked_vai(widget):
-    global train #1 para treino e 0 para classificação
+def clicked_train(widget):
     global path
+    handle_folder()
 
-    if train.value():
-        facebank = os.path.realpath("facenet/facebank")
-        print(path,facebank)
-        if (path != facebank):
-            print("entrou")
-            for filename in os.listdir(path):
-                copy_tree(path,facebank)
 
-        treino(facebank)
-        caixa.image(None)
-        caixa.label("Rede atualizada com sucesso.")
+    if (path == " "):
+        return
 
-    else:
-        convert_jpeg(path)
-        classificar(path)
-        texto = ""
-        with open("out.txt","r") as file:
-            for line in file:
-                texto = texto+line+"\n"
-        caixa.label(texto)
-        caixa.image(None)
+    facebank = os.path.realpath("facenet/facebank")
+    if (path != facebank):
+        for filename in os.listdir(path):
+            copy_tree(path,facebank)
+
+    treino(facebank)
+    caixa.label("Rede atualizada com sucesso.")
+
+
+def clicked_classify(widget):
+    global path
+    handle_folder()
+
+
+    if (path == " "):
+        return
+
+    convert_jpeg(path)
+    classificar(path)
+    texto = ""
+    with open("out.txt","r") as file:
+        for line in file:
+            texto = texto+line+"\n"
+    caixa.label(texto)
 
 def main():
     global caixa
     global train
+    global path
+    path = " "
+
     window = Fl_Window(600, 400)
     window.label("Facenet")
 
-    button1 = Fl_Button(400, 160, 120, 50)
-    button1.label("Abrir")
-    button1.callback(clicked_folder)
-
-    #button2 = Fl_Button(400, 200, 120, 20)
-    #button2.label("ARQUIVO")
-    #button2.callback(clicked_file)
-
-    train = Fl_Round_Button(370, 120, 20, 5, "Treinar")
-    classify = Fl_Round_Button(450, 120, 20, 5, "Classificar")
-    train.type(FL_RADIO_BUTTON)
-    classify.type(FL_RADIO_BUTTON)
+    button1 = Fl_Button(400, 130, 120, 50)
+    button1.label("Treinar")
+    button1.callback(clicked_train)
 
     caixa = Fl_Box(50,10,300,380)
     caixa.box(FL_UP_BOX)
-    caixa.label("Neste campo serão apresentadas \n imagens e informações \n de classificação do algoritmo.")
+    caixa.label("Neste campo serão apresentadas \n informações de treino e\n de classificação do algoritmo.")
 
 
-    button2 = Fl_Button(400, 240, 120, 20)
-    button2.label("Vai")
-    button2.callback(clicked_vai)
+    button2 = Fl_Button(400, 210, 120, 50)
+    button2.label("Classificar")
+    button2.callback(clicked_classify)
 
     window.end()
     window.show()
